a845732c5ee8ca92c148c6c80640674eb4521c1b
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-10-19 20:21:38 -0300

    Criação do plano de contas

--- server/api/core/models.py
+++ server/api/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class PlanOfAccounts(models.Model):
+    name = models.CharField(
+        max_lengt=150,
+    )
+    due_date = models.IntegerField(
+        verbose_name="Due Date",
+        validators=[
+            MaxValueValidator(31),
+            MaxValueValidator(1)
+        ]
+    )
+    parent = models.ForeignKey(
+        PlanOfAccounts,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='childs',
+    )
